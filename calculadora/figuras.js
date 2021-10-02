48bb27d6c396d5cb0a7fd45b77d9c9350c819329
Cristian Fedora <cristianoso19@gmail.com>
2021-10-02 16:13:46 -0500

    Implement calcularModa() function to calculate this average

--- calculadora/figuras.js
+++ calculadora/figuras.js
@@ -6,6 +6,52 @@ function calcularPromedios() {
    const moda = calcularModa();
    console.log("PROMEDIO: " + promedio);
    console.log("MEDIANA: " + mediana);
+   console.log("MODA: Valor " + moda.valor + ", se repite: " + moda.contar);
+}
+
+function calcularModa() {
+   let modaElementos = [];
+   let cuentaValor = {};
+   let contar = 0;
+   let valor = 0;
+
+   for (let i = 0; i < lista.length; i++) {
+      if (i != 0) {
+         if (lista[i - 1] == lista[i]) {
+            contar++;
+         } else {
+            cuentaValor = {
+               valor,
+               contar
+            };
+            modaElementos.push(cuentaValor);
+            contar = 1;
+            valor = lista[i];
+         }
+         if (i == lista.length - 1) {
+            cuentaValor = {
+               valor,
+               contar
+            };
+            modaElementos.push(cuentaValor);
+         }
+      } else {
+         contar = 1;
+         valor = lista[i]
+      }
+   }
+   modaElementos.sort(ordenaCuenta);
+   //retornamos el ultimo elemento del array ordenado
+   return modaElementos.at(-1);
+}
+
+function ordenaCuenta(a, b) {
+   if (a.contar < b.contar) {
+      return -1;
+   } else if (a.contar > b.contar) {
+      return 1;
+   }
+   return 0;
 }
 
 function calculaMediana() {
